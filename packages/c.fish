c1a671bdb53c08924c554f639b82001ab6cd8347
Powers, Joshua <dev@powerslabs.org>
2026-03-04 13:35:28 -0700

    _

--- packages/c.fish
+++ packages/c.fish
@@ -3,6 +3,6 @@
 set GCC      gcc gdb glibc
 set LLVM     llvm clang clang-analyzer clang-tools-extra lldb
 set LIBS     libstdc++ libstdc++-devel libdrm libdrm-devel libinput libinput-devel libxkbcommon libxkbcommon-devel libtool
-set LIBS2    libevdev libevdev-devel libuvdev libuvdev-devel glib glib-devel libconfig libconfig-devel libconfig++ libconfig++-devel
+set LIBS2    libevdev libevdev-devel libudev libudev-devel glib glib-devel libconfig libconfig-devel libconfig++ libconfig++-devel
 set BUILD    make cmake automake pkg-config autoconf binutils patch ninja meson diffutils binutils
 set PACKAGES (string collect $PACKAGES $GCC $LLVM $LIBS $LIBS2 $BUILD)
